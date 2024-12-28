c3f158188caa77b2cbb00a22ba387c24c7a7bf2e
Jan Willamowius <jan@willamowius.de>
2024-12-28 19:04:20 +0100

    fix typo

--- Package.swift
+++ Package.swift
@@ -4,21 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "UPCBarcodeLokup",
+    name: "UPCBarcodeLookup",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "UPCBarcodeLokup",
-            targets: ["UPCBarcodeLokup"]),
+            name: "UPCBarcodeLookup",
+            targets: ["UPCBarcodeLookup"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "UPCBarcodeLokup"),
+            name: "UPCBarcodeLookup"),
         .testTarget(
-            name: "UPCBarcodeLokupTests",
-            dependencies: ["UPCBarcodeLokup"]
+            name: "UPCBarcodeLookupTests",
+            dependencies: ["UPCBarcodeLookup"]
         ),
     ]
 )
